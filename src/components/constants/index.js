edaea8dd670cc511caad19906a12c3033e1377e5
Christian Simmons <csimmonscs@gmail.com>
2020-10-28 20:20:23 -0700

    Removed extra / from constants url

--- src/components/constants/index.js
+++ src/components/constants/index.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://soundsfamiliar-deploy.herokuapp.com//api/songs/";
-export const USER_URL = "https://soundsfamiliar-deploy.herokuapp.com//core/users/";
+export const API_URL = "https://soundsfamiliar-deploy.herokuapp.com/api/songs/";
+export const USER_URL = "https://soundsfamiliar-deploy.herokuapp.com/core/users/";
